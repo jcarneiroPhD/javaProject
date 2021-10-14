11486d37c1a0fa538883e08d60d29635b23663e3
João Carneiro <jcarneiro@estg.ipp.pt>
2021-10-14 22:02:30 +0100

    Funcionalidade de gestao de Pessoas

---
+++ EngSoft2/src/main/java/gradleExample/Person.java
@@ -0,0 +1,19 @@
+package gradleExample;
+
+public class Person {
+
+    private int idade;
+
+    public Person(int idade) {
+
+        this.setIdade(idade);
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
